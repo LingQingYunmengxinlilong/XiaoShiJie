d864b81e857ca7c7934bae6e2160e4ab0e436ed7
LingQingYunmengxinlilong <xsh09154953>
2016-07-16 12:27:37 +0800

    框架

--- app/src/main/java/com/meng/xue/xiaoshijie/fragment/HomePageFragement.java
+++ app/src/main/java/com/meng/xue/xiaoshijie/fragment/HomePageFragement.java
@@ -16,7 +16,7 @@ public class HomePageFragement extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.homepagefragement,null);
-
+          int a;
 
 
         return view;
